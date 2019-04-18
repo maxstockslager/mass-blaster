aa0be489a0a111de530f692018e24facde8f4daa
maxstockslager <max.stockslager@gmail.com>
2019-04-18 14:37:24 -0400

    fixed remaining bug related to reading in Google sheets as strings rather than numeric

--- helper functions/convert_field_to_numeric.m
+++ helper functions/convert_field_to_numeric.m
@@ -1,6 +1,7 @@
 function metadata_out = convert_field_to_numeric(metadata, fieldname)
 
-new_field = num2cell(str2num(cell2mat(metadata.(fieldname))));
+new_field = num2cell(cellfun(@(x) str2num(x), metadata.(fieldname)));
+% new_field = num2cell(str2num(cell2mat(metadata.(fieldname))));
 metadata_out = metadata;
 metadata_out.(fieldname) = new_field;
 end
