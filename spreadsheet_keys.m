f07b2267b29f8411c7b98738e416e4d8bb8d9742
maxstockslager <max.stockslager@gmail.com>
2019-08-13 08:34:53 -0400

    added baf3 spreadsheet to key list

--- spreadsheet_keys.m
+++ spreadsheet_keys.m
@@ -1,9 +1,11 @@
-function [key, gid] = spreadsheet_keys(system, sheet)
+function [key, gid] = SPREADSHEET_KEYS(system, sheet)
 
 switch system
     case 'MB1'
         key = '1ZGlRd2MdWTbD31BbJzbkGvpnMiY1y-pztS4rQq2zV6A';
         switch sheet
+            case '20190812-baf3'
+                gid = '815719910';
             case '20190325-GBM'
                 gid = '1426304271';
             case '20190325-Leukemia'
@@ -18,6 +20,8 @@ switch system
     case 'MB2'
         key = '1x3tMqMacX6RdZQtH0SXU7R_6uimEX3XrPt8fG8byvQ8';
         switch sheet
+            case '20190812-baf3'
+                gid = '973793667';
             case '20190325-GBM'
                 gid = '1426304271';
             otherwise 
